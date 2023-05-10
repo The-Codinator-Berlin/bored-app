8897d4adc589b0a373113b524e0f17038f4c83dd
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-10 16:44:55 +0200

    Landing page initial build

---
+++ src/LandingPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './Header';
+import MainContentLP from './MainContentLP';
+import Footer from './Footer';
+import './LandingPage.css';
+
+
+
+function LandingPage() {
+  return (
+    
+      <div>
+        <Header />
+        <MainContentLP />
+        <Footer />
+      </div>
+  )
+}
+
+export default LandingPage;

5ccba5bed2fd88e8eea0bb57f77f9364ec12e870
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-10 17:05:03 +0200

    Typo corrected

--- src/MainContentLP.jsx
+++ src/MainContentLP.jsx
@@ -3,7 +3,7 @@ import './index.css'
 function MainContentLP() {
   return (
     <div className="flexMainContent">
-      <div className="mainContentDiv text-sky-500 font-poppins font-normal text-center md:font-bold text-base">There's no need to be bored <br></br>wheen you can cure <br></br>your boredom!</div>
+      <div className="mainContentDiv text-sky-500 font-poppins font-normal text-center md:font-bold text-base">There's no need to be bored <br></br>when you can cure <br></br>your boredom!</div>
       <button className="rounded bg-rose-500 text-sky-300 font-light">Activity Generator button</button>
       <div className="generatorBox sm:flex border-dashed border-2 border-sky-500"> 
       <div className="favouritesButton"></div>

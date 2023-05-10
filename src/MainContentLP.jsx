ca03305706c19040d64304347f95e9f1898a7ea1
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-10 17:02:13 +0200

    Main content component added

---
+++ src/MainContentLP.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.css'
+function MainContentLP() {
+  return (
+    <div className="flexMainContent">
+      <div className="mainContentDiv text-sky-500 font-poppins font-normal text-center md:font-bold text-base">There's no need to be bored <br></br>wheen you can cure <br></br>your boredom!</div>
+      <button className="rounded bg-rose-500 text-sky-300 font-light">Activity Generator button</button>
+      <div className="generatorBox sm:flex border-dashed border-2 border-sky-500"> 
+      <div className="favouritesButton"></div>
+      </div>
+      </div>
+  )
+}
+
+export default MainContentLP;

9e9f383ac71064e09d208f77c1b6c02e51262af6
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-10 16:45:58 +0200

    Header with login button added

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex px-1.5 py-1.5">
+        <>login</>
+      <button><span className="material-symbols-outlined">Login</span></button>
+    </div>
+  );
+}
+
+export default Header;

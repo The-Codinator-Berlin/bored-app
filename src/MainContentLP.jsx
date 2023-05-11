f37f62da37a871583dc428c15a72eb09039969ec
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-11 13:55:58 +0200

    H1 added

--- src/MainContentLP.jsx
+++ src/MainContentLP.jsx
@@ -3,6 +3,7 @@ import './index.css'
 function MainContentLP() {
   return (
     <div className="flexMainContent">
+      <h1 className="text-5xl">Bored-App</h1>
       <div className="mainContentDiv text-sky-500 font-poppins font-normal text-2xl text-center md:font-bold">There's no need to be bored <br></br>when you can cure <br></br>your boredom!</div>
       <button className="rounded bg-rose-500 text-sky-300 font-light hover:bg-sky-500 hover:text-rose-500 active:bg-amber-500 active:text-green-600">Activity Generator button</button>
       <div className="generatorBox sm:flex border-dashed border-2 border-sky-500"> 

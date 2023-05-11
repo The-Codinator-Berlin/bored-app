773994e09c87e4a12ac42471bf9e1d90d8134d4d
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-11 13:16:35 +0200

    Added hover/active to button

--- src/MainContentLP.jsx
+++ src/MainContentLP.jsx
@@ -4,7 +4,7 @@ function MainContentLP() {
   return (
     <div className="flexMainContent">
       <div className="mainContentDiv text-sky-500 font-poppins font-normal text-center md:font-bold text-base">There's no need to be bored <br></br>when you can cure <br></br>your boredom!</div>
-      <button className="rounded bg-rose-500 text-sky-300 font-light">Activity Generator button</button>
+      <button className="rounded bg-rose-500 text-sky-300 font-light hover:bg-sky-500 hover:text-rose-500 active:bg-amber-500 active:text-green-600">Activity Generator button</button>
       <div className="generatorBox sm:flex border-dashed border-2 border-sky-500"> 
       <div className="favouritesButton"></div>
       </div>

4bdb549d01193d71dafc94bd9f090d58a922528e
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-11 10:24:35 +0200

    Added hover effects to login button

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+
+
 function Header() {
   return (
     <div className="flex px-1.5 py-1.5">
         <>login</>
-      <button><span className="material-symbols-outlined">Login</span></button>
+      <button><span className="hover:text-sky-500 active:text-amber-500 material-symbols-outlined">Login</span></button>
     </div>
   );
 }

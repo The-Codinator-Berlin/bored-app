e0091d7285584dc2b0a404c92275a35b0101137c
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-11 13:41:56 +0200

    Updated footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='sm:flex justify-center flex-wrap px-1.5 py-1.5 text-sm text-center'>&copy; 2023 Desgined by &nbsp; <a className="text-sky-500" href="https://github.com/The-Codinator-Berlin">Carl Manley</a> &nbsp; using data provided by /&nbsp; <a className="text-sky-500"href="https://www.boredapi.com/">Bored API</a></div>
+    <div className='sm:flex justify-center flex-wrap px-1.5 py-1.5 text-sm text-center'>&copy; 2023 Desgined by &nbsp;<a className="text-sky-500" href="https://github.com/The-Codinator-Berlin">Carl Manley</a>&nbsp;/ using data provided by &nbsp;<a className="text-sky-500"href="https://www.boredapi.com/">Bored API</a></div>
   )
 }
 
--- src/MainContentLP.jsx
+++ src/MainContentLP.jsx
@@ -3,7 +3,7 @@ import './index.css'
 function MainContentLP() {
   return (
     <div className="flexMainContent">
-      <div className="mainContentDiv text-sky-500 font-poppins font-normal text-center md:font-bold text-base">There's no need to be bored <br></br>when you can cure <br></br>your boredom!</div>
+      <div className="mainContentDiv text-sky-500 font-poppins font-normal text-2xl text-center md:font-bold">There's no need to be bored <br></br>when you can cure <br></br>your boredom!</div>
       <button className="rounded bg-rose-500 text-sky-300 font-light hover:bg-sky-500 hover:text-rose-500 active:bg-amber-500 active:text-green-600">Activity Generator button</button>
       <div className="generatorBox sm:flex border-dashed border-2 border-sky-500"> 
       <div className="favouritesButton"></div>

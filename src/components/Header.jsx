a12992bf1d66c68751f92549e9bc29ce35e425bd
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-15 14:25:14 +0200

    Padding adjusted to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="flex px-1.5 py-1.5 text-md">
+    <div className="flex px-1.5 py-0.5 text-md">
         <>login</>
       <button><span className="hover:text-sky-500 active:text-amber-500 material-symbols-outlined ">Login</span></button>
     </div>

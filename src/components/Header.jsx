1212435a77283c6120f2ee601aa148f4e18111c6
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-15 13:20:40 +0200

    Organized files into folders

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function Header() {
+  return (
+    <div className="flex px-1.5 py-1.5 text-md">
+        <>login</>
+      <button><span className="hover:text-sky-500 active:text-amber-500 material-symbols-outlined ">Login</span></button>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/views/LandingPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Header';
+import MainContentLP from '../components/MainContentLP';
+import Footer from '../components/Footer';
+import '../LandingPage.css';
+
+
+
+function LandingPage() {
+  return (
+    
+      <div>
+        <Header />
+        <MainContentLP />
+        <Footer />
+      </div>
+  )
+}
+
+export default LandingPage;
